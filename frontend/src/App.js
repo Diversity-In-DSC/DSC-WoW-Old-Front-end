2ec44bc0ea178193930460deb5cbe60552f96025
Sanchi Bansal <sanchibansal340@gmail.com>
2020-10-13 01:46:51 +0530

    Create timeline section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
       {/* <AboutUs /> */}
       {/* <Speaker /> */}
       {/* <Sponsor /> */}
-      {/* <Timeline /> */}
+      <Timeline /> 
       {/* <Contact /> */}
       <Faq />
       <Footer />
--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import TimeImage from '../assets/timeline.png'
+import '../css/timeline.css'
 
 function Timeline() {
     return (
-        <div>
-            timeline
+        <div className="faq_sec my-5">
+            <div className="row">
+                <div className="col-md-7">
+                    <div className="timeline">
+                        <h2 className='font-italic'>Timeline</h2>
+                        <br/>
+                        <p>Interested in knowing more about our DSC WOW. <br/>
+                            See the flow of our events over here.
+                        </p>
+                    </div>
+                    
+                </div>
+                <div className="col-12 col-md-5">
+                        <img src={TimeImage} alt="Timeline" className="img-fluid w-75" />
+                </div>
+            </div>
+
+            {/* ------- Timeline cards -------- */}
+            <div className='container-fluid tl-cards col-md mt-4'>
+                <div className='row'>
+
+                    {/* ------- Day 1 card -------- */}
+                    <div className="card mr-5 mb-3" style={{width:"300px", height:"250px"}}>
+                        <h5 className="card-header text-center text-white" style={{background: "#FE4F5A"}}>
+                            Day 1
+                        </h5>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Android Day</h5>
+                            <p className="card-text">
+                                Learn about Android App Development from the best of the speakers in the industry!
+                            </p>
+                            <a href="#" class="btn btn-primary" style={{background: "#FE4F5A"}}>
+                                Know More
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* ------- Day 2 card -------- */}
+                    <div className="card mr-5 mb-3" style={{width:"300px", height:"250px"}}>
+                        <h5 className="card-header text-center text-white" style={{background: "#FFBD00"}}>
+                            Day 2
+                        </h5>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Cloud Day</h5>
+                            <p className="card-text">
+                                Learn about Google Cloud Platform from the best of the speakers in the industry!
+                            </p>
+                            <a href="#" class="btn btn-primary" style={{background: "#FFBD00"}}>
+                                Know More
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* ------- Day 3 card -------- */}
+                    <div className="card mr-5 mb-3" style={{width:"300px", height:"250px"}}>
+                        <h5 className="card-header text-center text-white" style={{background: "#546DDD"}}>
+                            Day 3
+                        </h5>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">AI/ML Day</h5>
+                            <p className="card-text">
+                                Learn about AI and ML from the best of the speakers in the industry!                                        
+                            </p>
+                            <a href="#" class="btn btn-primary" style={{background: "#546DDD"}}>
+                                Know More
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* ------- Day 4 card -------- */}
+                    <div className="card mr-5 mb-3" style={{width:"300px", height:"250px"}}>
+                        <h5 className="card-header text-center text-white" style={{background: "#00A452"}}>
+                                    Day 4
+                        </h5>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Web Day</h5>
+                            <p className="card-text">
+                                Learn about Web Development from the best of the speakers in the industry! 
+                            </p>
+                            <a href="#" class="btn btn-primary" style={{background: "#00A452"}}>
+                                Know More
+                            </a>
+                        </div>
+                    </div>
+                </div>   
+            </div>
         </div>
     )
 }
