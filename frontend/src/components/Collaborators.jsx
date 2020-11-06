347d50449f00614d9e8a28c6a0a564526d4b9dc9
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-11-06 17:01:31 +0530

    Update Collaborators.jsx

--- frontend/src/components/Collaborators.jsx
+++ frontend/src/components/Collaborators.jsx
@@ -6,7 +6,7 @@ export default function Collaborating() {
         <div className="collab__main">
             <h1>Collaborators</h1>
             <p>DSC collaborating in DSC WOW</p>
-            <iframe title="DSC Collaborating in DSC WOW" src="https://www.google.com/maps/d/embed?mid=1F9Lyn8xgdMJwGUWr-BC1rjKVVeZTlv3A" width="100%" height="480"></iframe>
+            <iframe title="DSC Collaborating in DSC WOW" src="https://www.google.com/maps/d/embed?mid=14PPtnaowpoAyKNwzowXXCh0zukhzYuL5" width="100%" height="480"></iframe>
         </div>
     )
 }
