cf892af28a0a5c57ca0a8ece2c072fff0bcc52b6
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-11-10 10:07:10 +0530

    Changed width of i frame

--- src/components/Collaborators.jsx
+++ src/components/Collaborators.jsx
@@ -6,7 +6,7 @@ export default function Collaborating() {
         <div className="collab__main">
             <h1>Collaborators</h1>
             <p>"Great things are never done by one person. They're done by a team of people."</p>
-            <iframe title="DSC Collaborating in DSC WOW" src="https://www.google.com/maps/d/embed?mid=14PPtnaowpoAyKNwzowXXCh0zukhzYuL5" width="100%" height="480"></iframe>
+            <iframe title="DSC Collaborating in DSC WOW" src="https://www.google.com/maps/d/embed?mid=14PPtnaowpoAyKNwzowXXCh0zukhzYuL5" width="100%" height="680"></iframe>
         </div>
     )
 }
