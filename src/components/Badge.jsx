515a15df4e34d19d4f93395d58ef6fc3fb34909b
suyashsonawane <suyashsonawane007@gmail.com>
2020-11-13 15:30:06 +0530

    changed styling

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "@tensorflow/tfjs-backend-cpu";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 import frame from "../assets/frame-light.png";
 import dumb_face from "../assets/face-placeholder.png";
-import logo from "../assets/dscwowF5F5F5.gif"
-import '../css/badge.css'
+import "../css/badge.css";
 
 const blazeface = require("@tensorflow-models/blazeface");
 
@@ -25,26 +24,27 @@ const BadgeModule = () => {
     load();
 
     async function load() {
-
         model = await blazeface.load();
         // setStatus("Model Loaded");
 
-        const style = 'font-weight: bold; font-size: 30px;color: yellow; text-shadow: 3px 3px 0 rgb(217,31,38) ;padding : 50px ';
+        const style =
+            "font-weight: bold; font-size: 30px;color: yellow; text-shadow: 3px 3px 0 rgb(217,31,38) ;padding : 50px ";
 
-        console.log('%c Hi You have found an easter egg 🥚, now find a way to report this to us!', style);
+        console.log(
+            "%c Hi You have found an easter egg 🥚, now find a way to report this to us!",
+            style
+        );
 
         if (!loaded) {
             setTimeout(() => {
                 let badge = document.getElementById("badge-canvas");
-                console.log(badge)
+                console.log(badge);
                 let badgectx = badge.getContext("2d");
                 let frame = document.getElementById("frame");
                 let dumb_face = document.getElementById("dumb-face");
                 badgectx.drawImage(dumb_face, 0, 0, badge.width, badge.height);
                 badgectx.drawImage(frame, 0, 0, badge.width, badge.height);
-
             }, 1000);
-
         }
         setLoaded(true);
     }
@@ -57,40 +57,46 @@ const BadgeModule = () => {
     }
 
     async function run() {
-        let img = document.querySelector("#temp-image")
+        let img = document.querySelector("#temp-image");
 
         if (img.width === 0 || img.height === 0) {
-            toast.error("Something is wrong", { position: "bottom-center", autoClose: 3000 })
-            return
+            toast.error("Something is wrong", {
+                position: "bottom-center",
+                autoClose: 3000,
+            });
+            return;
         }
 
         if (img.naturalWidth < 500 || img.naturalHeight < 500) {
-            toast.warn("Low resolution image, resizing badge to low dimensions")
-            setCanvasDim({ width: img.naturalWidth, height: img.naturalWidth })
-
+            toast.warn("Low resolution image, resizing badge to low dimensions");
+            setCanvasDim({ width: img.naturalWidth, height: img.naturalWidth });
         }
-        let results = await model.estimateFaces(
-            img,
-            false
-        );
+        let results = await model.estimateFaces(img, false);
         if (results[0]) {
-            toast.success("shinning like a star ⭐")
+            toast.success("shinning like a star ⭐");
             let result = results[0];
-            let x = parseFloat(result.topLeft[0]) - 0.8 * (parseFloat(result.bottomRight[0]) - parseFloat(result.topLeft[0]))
-            let y = parseFloat(result.topLeft[1]) - 0.5 * (parseFloat(result.bottomRight[1]) - parseFloat(result.topLeft[1]))
+            let x =
+                parseFloat(result.topLeft[0]) -
+                0.8 *
+                (parseFloat(result.bottomRight[0]) - parseFloat(result.topLeft[0]));
+            let y =
+                parseFloat(result.topLeft[1]) -
+                0.5 *
+                (parseFloat(result.bottomRight[1]) - parseFloat(result.topLeft[1]));
             // console.log(x, y);
             setCrop({
                 x: x,
                 y: y,
                 aspect: 1,
-                width: (parseFloat(result.bottomRight[0]) - parseFloat(result.topLeft[0])) * 2.5,
+                width:
+                    (parseFloat(result.bottomRight[0]) - parseFloat(result.topLeft[0])) *
+                    2.5,
                 // height: (parseFloat(result.bottomRight[1]) - parseFloat(result.topLeft[1])) * 2.5
             });
 
             setReady(true);
-        }
-        else {
-            toast.warn("Can't detect your face 😞")
+        } else {
+            toast.warn("Can't detect your face 😞");
             setCrop({
                 x: 0,
                 y: 0,
@@ -101,12 +107,11 @@ const BadgeModule = () => {
 
             setReady(true);
         }
-
     }
 
     const getCroppedImg = () => {
         setReady(false);
-        setShowLoader(false)
+        setShowLoader(false);
         const canvas = document.createElement("canvas");
         const scaleX = image.naturalWidth / image.width;
         const scaleY = image.naturalHeight / image.height;
@@ -136,32 +141,38 @@ const BadgeModule = () => {
             badgectx.clearRect(0, 0, canvas.width, canvas.height);
             badgectx.drawImage(img, 0, 0, badge.width, badge.height);
             badgectx.drawImage(frame, 0, 0, badge.width, badge.height);
-            setSuccess(badge.toDataURL("image/jpeg"))
+            setSuccess(badge.toDataURL("image/jpeg"));
         };
         img.src = canvas.toDataURL("image/jpeg");
 
-        toast.success("psst, your badge is ready to download ", { position: "top-right", autoClose: 3000 })
+        toast.success("psst, your badge is ready to download ", {
+            position: "top-right",
+            autoClose: 3000,
+        });
     };
 
     function uploadImage() {
-        setShowLoader(true)
+        setShowLoader(true);
         document.querySelector("input.profile-input").click();
     }
     function refreshPage() {
         window.location.reload(false);
     }
     function adjust() {
-        setReady(true)
-        setSuccess(null)
+        setReady(true);
+        setSuccess(null);
     }
     return (
         <>
-
             <div className={loaded ? "done" : "overlay"}>
                 <span>
                     <h3>Loading Model</h3>
-                    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-
+                    <div className="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </span>
             </div>
 
@@ -179,37 +190,54 @@ const BadgeModule = () => {
             />
 
             <div className="main-container">
-
                 <div className="input-panel">
-
                     <div className="input">
                         <label>Profile Picture</label>
                         <span>
-                            {success && <button className="button" onClick={refreshPage}>New Image</button>}
-                            {
-                                !success && <button className="button" onClick={uploadImage}>Upload Image</button>
-                            }
+                            {success && (
+                                <button className="button" onClick={refreshPage}>
+                                    New Image
+                                </button>
+                            )}
+                            {!success && (
+                                <button className="button" onClick={uploadImage}>
+                                    Upload Image
+                                </button>
+                            )}
 
-                            {showLoader && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
+                            {showLoader && (
+                                <div className="lds-ring">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            )}
                         </span>
-                        {loaded && <input type="file" accept="image/*" onChange={handleFile} className="profile-input" hidden />}
+                        {loaded && (
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={handleFile}
+                                className="profile-input"
+                                hidden
+                            />
+                        )}
                         {/* <h4>Status : {status}</h4> */}
 
                         <br />
                         <h5>
-                            An AI Powered Badge Maker to automatically suggest crop region depending on the face location. <br /> <br />
+                            An AI Powered Badge Maker to automatically suggest crop region
+              depending on the face location. <br /> <br />
               Powered by TensorFlow.js <br /> <br />
               Build with ❤️ by DSC WOW Team
-
             </h5>
                     </div>
 
                     {/* <img src={logo} className="logo" alt="DSC WOW Logo" /> */}
-
                 </div>
 
                 <div className="preview-panel">
-
                     {ready && (
                         <div>
                             <div style={{ maxWidth: "450px", width: "80vw" }}>
@@ -220,11 +248,17 @@ const BadgeModule = () => {
                                     onChange={(newCrop) => setCrop(newCrop)}
                                 />
                             </div>
-                            <button onClick={getCroppedImg} className="button">Crop</button>
+                            <button onClick={getCroppedImg} className="button">
+                                Crop
+              </button>
                         </div>
                     )}
 
-                    <canvas id="badge-canvas" width={canvasDim.width} height={canvasDim.height}></canvas>
+                    <canvas
+                        id="badge-canvas"
+                        width={canvasDim.width}
+                        height={canvasDim.height}
+                    ></canvas>
                     {file && (
                         <img
                             src={file}
@@ -243,19 +277,27 @@ const BadgeModule = () => {
                     <img src={frame} id="frame" alt="" hidden />
                     <img src={dumb_face} id="dumb-face" alt="" hidden />
 
-                    {success && <button className="button adjust" onClick={adjust}>Adjust Crop</button>}
-                    {success &&
+                    {success && (
+                        <button className="button adjust" onClick={adjust}>
+                            Adjust Crop
+                        </button>
+                    )}
+                    {success && (
                         <a download="myBadge.jpg" href={success}>
                             <div className="download-fab">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#fff">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    width="24"
+                                    fill="#fff"
+                                >
                                     <path d="M0 0h24v24H0V0z" fill="none"></path>
                                     <path d="M16.59 9H15V4c0-.55-.45-1-1-1h-4c-.55 0-1 .45-1 1v5H7.41c-.89 0-1.34 1.08-.71 1.71l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.63-.63.19-1.71-.7-1.71zM5 19c0 .55.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1H6c-.55 0-1 .45-1 1z"></path>
                                 </svg>
                             </div>
                         </a>
-                    }
-
-
+                    )}
                 </div>
             </div>
         </>
